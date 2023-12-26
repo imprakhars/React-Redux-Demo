5e4feee76d0b85d0d21551b37407d77b2c477afb
Srivastava <2308309@cognizant.com>
2023-12-26 19:36:14 +0530

    First commit for redux project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Test1 from './components/Test1';
+import Test2 from './components/Test2';
+import { useSelector, useDispatch } from 'react-redux';
+import { incrementCounter } from './slices/counterSlice';
 
 function App() {
+
+  let counterObj = useSelector(state=>state.counterState);
+  let dispatch = useDispatch();
+
+  function changeCounter(){
+    //call action creator function
+    let actionObj = incrementCounter();
+
+    //dispatch action obj to store
+    dispatch(actionObj);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='p-5 bg-primary m-4 text-center'>
+      <p className='display-3 text-white'>Root component</p>
+      <p className='lead fs-1 text-warning'>{counterObj.counter}</p>
+      <button className='btn btn-warning' onClick={changeCounter}>Increment</button>
+      <div className='row '>
+        <div className='col-sm-4 p-4 bg-secondary m-4 mx-auto'>
+          <Test1 />
+        </div>
+        <div className='col-sm-4 p-4 bg-secondary m-4 mx-auto'>
+          <Test2 /> 
+        </div>
+      </div>
     </div>
   );
 }
